078d3cd91870b0783c76f028a3a916188ed14e8b
yihannangua <54133432+yihannangua@users.noreply.github.com>
2022-11-17 18:38:50 +0800

    更新nodejs代码

--- Webcode/Node/10 大事件案例/api_server/router/artcate.js
+++ Webcode/Node/10 大事件案例/api_server/router/artcate.js
@@ -12,9 +12,19 @@ router.get('/cates',artcate_handler.getArticleCates);
 
 
 //导入需要的验证规则对象
-const { artcate_schema } = require('../schema/artcate');
+const { artcate_schema,deletecate_schema,getCate_schema,updateCate_schema} = require('../schema/artcate');
 
 //新增文章分类
 router.post('/addcates',expressJoi(artcate_schema),artcate_handler.addCates);
+
+//根据id删除文章分类
+router.get('/deletecate/:id',expressJoi(deletecate_schema),artcate_handler.deleteCate);
+
+//根据id查询文章分类
+router.get('/cates/:id',expressJoi(getCate_schema),artcate_handler.getArtCateById);
+
+//根据id更新文章分类
+router.post('/updatecate',expressJoi(updateCate_schema),artcate_handler.updateCate);
+
 
 module.exports = router;
--- Webcode/Node/10 大事件案例/api_server/schema/artcate.js
+++ Webcode/Node/10 大事件案例/api_server/schema/artcate.js
@@ -5,9 +5,32 @@ const joi = require('joi');
 const name = joi.string().required();
 const alias = joi.string().alphanum().required();
 
+
 //定义验证文章分类名和类别的规则对象
 exports.artcate_schema = {
     body: {
+        name,
+        alias,
+    },
+}
+
+const id = joi.number().integer().min(1).required();
+
+exports.deletecate_schema = {
+    params: {
+        id,
+    },
+}
+
+exports.getCate_schema = {
+    params: {
+        id,
+    },
+}
+
+exports.updateCate_schema = {
+    body: {
+        id,
         name,
         alias,
     },
