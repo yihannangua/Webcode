9774da761c5fe0b93e49877e5753ab19c74005f4
yihannangua <54133432+yihannangua@users.noreply.github.com>
2022-11-18 10:25:39 +0800

    更新nodejs代码

--- Webcode/Node/10 大事件案例/api_server/app.js
+++ Webcode/Node/10 大事件案例/api_server/app.js
@@ -45,6 +45,10 @@ app.use('/my',userInfoRouter);
 const artCateRouter = require('./router/artcate');
 app.use('/my/article',artCateRouter);
 
+//导入并使用文章路由模块
+const articleRouter = require('./router/article');
+app.use('/my/article',articleRouter);
+
 //定义错误级别的中间件
 app.use((err,req,res,next) => {
     //验证失败的错误
