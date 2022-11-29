4777d6bfd65e88cd7698bd0e7feb34725014cbf1
yihannangua <54133432+yihannangua@users.noreply.github.com>
2022-11-29 21:10:56 +0800

    更新es6代码

---
+++ Webcode/JS面向对象+ES6/07 类案例/js/tab.js
@@ -0,0 +1,62 @@
+var that;
+class Tab {
+    constructor(id) {
+        that = this;
+        //获取元素
+        this.main = document.querySelector(id);
+        this.ul = that.main.querySelector('ul');
+        this.tabadd = this.main.querySelector('.tabadd');
+        this.tabscon = this.main.querySelector('.tabscon');
+        this.init();
+    }
+    init() {
+        //init初始化操作让相关的元素绑定事件
+        this.updateNode();//重新获取元素
+        for(var i = 0;i < this.lis.length;i++) {
+            this.lis[i].index = i;
+            this.lis[i].onclick = this.toggleTab;
+        }
+        this.tabadd.onclick =  this.addTab;
+    }
+    //1.切换功能
+    toggleTab() {
+        // console.log(this.index);
+        that.clearClass();
+        this.className = 'liactive';
+        that.sections[this.index].className = 'conactive';
+    }
+    //2.添加功能
+    addTab() {
+        // console.log(this);
+        //创建li元素和section元素
+        that.clearClass();
+        var x = Math.random();
+        var li = '<li  class="liactive"><span>新选项卡</span><span class="iconfont icon-guanbi"></span></li>'
+        that.ul.insertAdjacentHTML('beforeend',li);
+        var section = '<section class="conactive">新内容 '+ x +'</section>';
+        that.tabscon.insertAdjacentHTML('beforeend',section);
+        that.init();//再次执行初始化，保证新添加的元素被获取并绑定事件
+    }
+    //3.移除功能
+    removeTab() {
+
+    }
+    //4.编辑功能
+    editTab() {
+
+    }
+    //循环清除样式
+    clearClass() {
+        for(var i = 0;i < this.lis.length;i++) {
+            this.lis[i].className = '';
+            this.sections[i].className = '';
+        }
+    }
+    //获取所有的li和section的元素
+    updateNode() {
+        this.lis = this.main.querySelectorAll('li');
+        this.sections = this.main.querySelectorAll('section');
+    }
+}
+
+new Tab('#tab');
